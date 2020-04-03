67f278ea88a31b9db01ae71fa6663c6d9b278349
Noud de Brouwer <noud4@home.nl>
2020-04-03 22:53:15 +0200

    hhvm-start

--- bin/hhvm-start.sh
+++ bin/hhvm-start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+
+# https://www.tecmint.com/install-hhvm-and-nginx-apache-with-mariadb-on-debian-ubuntu/
 sudo update-alternatives --install /usr/bin/php php /usr/bin/hhvm 60
 sudo systemctl start hhvm
